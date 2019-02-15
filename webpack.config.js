1d01299b63db962847583265d47ee62123fcc058
Saloni Kathuria <kathuria.saloni29@gmail.com>
2019-02-16 01:29:14 +0530

    dotenv parsing added

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,51 @@
 // enter entry and out files
 const path = require('path');
-module.exports = {
-    
-    entry : ['@babel/polyfill', './src/app.js'],
-    output : {
-        path : path.join(__dirname, 'public'), //this is the abs path on machine you want o/p of that webpack
-        filename : 'bundle.js' 
-    },
-    performance: {
-        hints: false
-    },
-    module : {
-        rules : [{
-            loader : 'babel-loader',
-            test : /\.js$/, // run through all the js files in your application
-            exclude : /node_modules/
-        },{
-            test : /\.s?css$/,
-            use : [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-            
-        }]
-    },
-    devtool : 'cheap-module-eval-source-map',
-    devServer : {
-        contentBase :  path.join(__dirname, 'public')
+const webpack = require('webpack');
+const dotenv = require('dotenv');
+const env = dotenv.config().parsed;
+
+
+// reduce it to a nice object, the same as before
+const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
+    return prev;
+}, {});
+
+module.exports = () => {
+
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'), //this is the abs path on machine you want o/p of that webpack
+            filename: 'bundle.js'
+        },
+        performance: {
+            hints: false
+        },
+        node: {
+            fs: "empty"
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/, // run through all the js files in your application
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+
+            }]
+        }, plugins:[
+            new webpack.DefinePlugin(envKeys)
+        ],
+        devtool: 'cheap-module-eval-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public')
+        }
     }
 }
+
