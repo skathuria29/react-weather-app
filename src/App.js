4d09d38cbd4190b39a1856779fefafcc17337996
Saloni Kathuria <kathuria.saloni29@gmail.com>
2019-02-16 01:36:09 +0530

    polyfill added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import '@babel/polyfill';
 
 import WeatherApp from './components/WeatherApp';
 // import 'normalize.css/normalize.css'; //all browsers work on same styles base
